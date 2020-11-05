93b2ded04715b920057a6e7374cad4bd66502720
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-11-05 11:30:52 -0600

    feat: adding css to my search bar.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <div className="weather-card">
-        <input
-          type="text"
-          className="search-bar"
-          placeholder="Search a city"
-        />
-      </div>
+        <main>
+          <div className="weather-card">
+              <div className="search-box">
+                  <input
+                      type="text"
+                      className="search-bar"
+                      placeholder="Search a city"
+                  />
+              </div>
+          </div>
+        </main>
     </div>
   );
 }
